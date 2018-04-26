f604cae873a519376df0ed3a8d61daa9efef6179
InnoFang <innofang@outlook.com>
2018-04-26 20:55:09 +0800

    :tada: initialization

---
+++ src/main/java/io/innofang/knockknock/KnockKnockApplication.java
@@ -0,0 +1,12 @@
+package io.innofang.knockknock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockKnockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockKnockApplication.class, args);
+	}
+}
---
+++ src/test/java/io/innofang/knockknock/KnockKnockApplicationTests.java
@@ -0,0 +1,16 @@
+package io.innofang.knockknock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnockKnockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
