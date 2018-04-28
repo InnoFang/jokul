307ad74d9e3135e5b323c2a234bf41c220957c8a
InnoFang <innofang@outlook.com>
2018-04-28 17:52:20 +0800

    :sparkles: implement user controller

---
+++ src/main/java/io/innofang/knockknock/controller/UserController.java
@@ -0,0 +1,47 @@
+package io.innofang.knockknock.controller;
+
+import io.innofang.knockknock.domain.Result;
+import io.innofang.knockknock.domain.User;
+import io.innofang.knockknock.enums.ResultEnum;
+import io.innofang.knockknock.repositories.UserRepository;
+import io.innofang.knockknock.utils.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Inno Fang on 2018/4/28.
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(value = "/user/sign_up")
+    public Result singUp(@RequestParam("user_name") String username,
+                         @RequestParam("user_password") String password) {
+        if (null != userRepository.findByUsername(username)) {
+            return ResultUtil.error(ResultEnum.USER_DUPLICATE);
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        userRepository.save(user);
+        return ResultUtil.success(ResultEnum.SIGN_UP);
+    }
+
+    @PostMapping(value = "/user/sign_in")
+    public Result signIn(@RequestParam("user_name") String usernamne,
+                         @RequestParam("user_password") String password) {
+        User user = userRepository.findByUsername(usernamne);
+        if (null == user) {
+            return ResultUtil.error(ResultEnum.USER_MISSED);
+        }
+        if (!user.getPassword().equals(password)) {
+            return ResultUtil.error(ResultEnum.WRONG_PASSWORD);
+        }
+        return ResultUtil.success(ResultEnum.SIGN_IN);
+    }
+}
