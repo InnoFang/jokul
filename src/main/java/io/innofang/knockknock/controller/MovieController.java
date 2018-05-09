a0eecbb9d109543a2fb8d54bd1dac1d735f43473
InnoFang <innofang@outlook.com>
2018-05-09 22:26:16 +0800

    :sparkles:

--- src/main/java/io/innofang/knockknock/controller/MovieController.java
+++ src/main/java/io/innofang/knockknock/controller/MovieController.java
@@ -7,6 +7,9 @@ import io.innofang.knockknock.exception.MovieNotFoundException;
 import io.innofang.knockknock.repositories.MovieRepository;
 import io.innofang.knockknock.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +25,11 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
-    @GetMapping(value = "/movieinfolist")
-    public Result<List<Movie>> getMovieInfoList() {
-        List<Movie> movies = movieRepository.findAll();
-        return ResultUtil.success(ResultEnum.GET_MOVIE_INFO_LIST, movies);
+    @GetMapping(value = "/movieinfolist/{page}")
+    public Result<List<Movie>> getMovieInfoList(@PathVariable("page") int page) {
+        Pageable pageable = PageRequest.of(page, 12);
+        Page<Movie> movies = movieRepository.findAll(pageable);
+        return ResultUtil.success(ResultEnum.GET_MOVIE_INFO_LIST, movies.getContent());
     }
 
     @GetMapping(value = "/{title}")
