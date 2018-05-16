eea7c4bf150ea0dc971efe71321ad6e85d8352a3
InnoFang <innofang@outlook.com>
2018-05-16 23:06:36 +0800

    :art: optimize it

--- client/public/canvas-nest.js
+++ client/public/canvas-nest.js
@@ -17,7 +17,7 @@
             z: get_attribute(script, "zIndex", -1), //z-index
             o: get_attribute(script, "opacity", 0.5), //opacity
             c: get_attribute(script, "color", "0,0,0"), //color
-            n: get_attribute(script, "count", 99) //count
+            n: get_attribute(script, "count", 200) //count
         };
     }
     //设置canvas的高宽
